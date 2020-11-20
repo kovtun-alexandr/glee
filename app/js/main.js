832fdbd0e2943b73ea311f7f329cbe0f639ebada
kovtun_a <kovtunalexander.84@gmail.com>
2020-11-20 15:35:03 +0200

    added products week

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,5 @@
 $(function(){
+
   $('.main-slider__inner').slick({
     dots:true,
     arrows:false,
@@ -6,4 +7,7 @@ $(function(){
     autoplay:true,
     autoplaySpeed: 2000
   })
+
+  var mixer = mixitup('.products-week__inner');
+
 });
