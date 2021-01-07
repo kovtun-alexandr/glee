17bcdc3e1afcc2fb6e0f7d419dc83d97232643f6
kovtun_a <kovtunalexander.84@gmail.com>
2021-01-07 15:27:26 +0200

    added terms-conditions adaptive and moduls

--- gulpfile.js
+++ gulpfile.js
@@ -107,7 +107,8 @@ function svgSpriteBuild(){
 function build(){
   return src([
     'app/**/*.html',
-    'app/css/style.min.css',
+    'app/css/*.min.css',
+    'app/fonts/*.*',
     'app/js/main.min.js'
   ], {base: 'app'})
     .pipe(dest('dist'))
